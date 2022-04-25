a356da2151ea2ad2d18eddacd19464a20fa1a34a
aayush <aayush.agarwal28@gmail.com>
2022-04-25 16:37:47 +0530

    Updates

---
+++ precache-manifest.907118a969fd09866633c06f42384003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0785b8cc09960d580ad46b2c33474aa9",
+    "url": "/corporatePortal/index.html"
+  },
+  {
+    "revision": "7ad8d0b5758504e2f176",
+    "url": "/corporatePortal/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "30b900f145d4800dad43",
+    "url": "/corporatePortal/static/css/main.5f678ca9.chunk.css"
+  },
+  {
+    "revision": "7ad8d0b5758504e2f176",
+    "url": "/corporatePortal/static/js/2.fcaa4a97.chunk.js"
+  },
+  {
+    "revision": "30b900f145d4800dad43",
+    "url": "/corporatePortal/static/js/main.d9a172fa.chunk.js"
+  },
+  {
+    "revision": "144ca181a83acf92806a",
+    "url": "/corporatePortal/static/js/runtime-main.eb7e5c44.js"
+  }
+]);
